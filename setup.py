01ae239e582a22063f1efe9fb85d4a8c5fe26547
schlitzer <stephan.schultchen@gmail.com>
2015-06-19 12:38:04 +0200

    fix setup.py crc16 dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_redis',
-    version='0.0.3',
+    version='0.0.4',
     description='Redis Client',
     long_description="""
 Redis Client implementation for Python 3.
