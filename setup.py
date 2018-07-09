e687c518357430e41963e2fdde5ce194d50a0006
Stephan Schultchen <stephan.schultchen@gmail.com>
2018-07-09 07:22:04 +0200

    performance improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_redis',
-    version='0.2.0',
+    version='0.2.1',
     description='Redis Client',
     long_description="""
 Redis Client implementation for Python 3.
